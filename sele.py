3784a8c8c8a79a2b8e77837e190a7c12d112e9b4
88bq <68809404+88bq@users.noreply.github.com>
2023-02-06 16:07:47 +0300

    Update sele.py

--- sele.py
+++ sele.py
@@ -1,98 +1,165 @@
-from os import system
-try:
-    import selenium
-except:
-    system('pip install selenium==4.0.0')
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-# Start a web driver (e.g. Chrome) and log in to Instagram
-users = []
-from selenium import webdriver
-
+import requests
+import pyttsx3
 options = webdriver.ChromeOptions()
-options.binary_location = '/usr/bin/chromium-browser'
 options.add_argument('--disable-extensions')
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
-options.add_argument('start-maximized')
-options.add_argument('disable-infobars')
-options.add_argument('--remote-debugging-port=9222')
-#options.binary_location = '/usr/bin/google-chrome'
+port = 8888 # or any other available port
+def read_aloud(text):
+    engine = pyttsx3.init()
+    engine.say(text, 'koky')
 
-driver = webdriver.Chrome("/usr/bin/chromedriver",chrome_options=options)
-print('ok')
-driver.get("https://www.instagram.com/accounts/login/")
-sleep(3)
-username_input = driver.find_element_by_name("username")
-password_input = driver.find_element_by_name("password")
-username_input.send_keys("zxcv.kis")
-password_input.send_keys("ahmedcan3")
-password_input.submit()
-sleep(8)
-print('[+] Done Login')
-# Go to the target user's profile
-driver.get("https://www.instagram.com/mn_tezy/")
-sleep(10)
-# Click on the following button
-following_button = driver.find_element_by_xpath('//a[@href="/mn_tezy/followers/"]')
-following_button.click()
-sleep(4)
-try:
-    popup = driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]')
-    a = 0
+    engine.runAndWait()
+
+
+#driver3 = webdriver.Chrome(executable_path="driver/chromedriver.exe")
+user = 'zxcv.kis'
+pas = 'ahmedcan3'
+user1 = []
+check1 = ['magedbmw29']
+
+def check(use):
+    url = 'https://www.instagram.com/api/v1/web/accounts/web_create_ajax/attempt/'
+    #user1 = open('kok.txt', 'r').read().split('\n')
+
+    data = {
+        'email':f'{use}@gmail.com',
+        'username': 'jkjrjgkkdgd',
+            'first_name':'koky',
+    'opt_into_one_tap':'false',
+    }
+    head = {
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-length': '55',
+        'content-type': 'application/x-www-form-urlencoded',
+        'cookie': 'datr=5WvSYy7FPvL1yCXXUNnrIUR7; csrftoken=eniA5Uj1P2k1kMzrfuSTZjkTzthjJD8I; mid=Y9Jr8wALAAF8wJLzi7GSWgm1PCal; ig_did=6E2F1F10-2048-4C38-ACF3-3F8301A2D440; ig_nrcb=1',
+        'origin': 'https://www.instagram.com',
+        'referer': 'https://www.instagram.com/accounts/emailsignup/',
+        'sec-ch-prefers-color-scheme': 'light',
+        'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        'viewport-width': '479',
+        'x-asbd-id': '198387',
+        'x-csrftoken': 'eniA5Uj1P2k1kMzrfuSTZjkTzthjJD8I',
+        'x-ig-app-id': '936619743392459',
+        'x-ig-www-claim': '0',
+        'x-instagram-ajax': '1006866983',
+        'x-requested-with': 'XMLHttpRequest',
+    }
+    q = requests.post(url, data=data, headers=head).json()['errors']
+
     try:
-        while a < 3:
-            sleep(3)
-            driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', popup)
-            a += 1
-        x = driver.page_source
-        user = str(x).split('aria-disabled="false"')
-        i = 4
-        open('user1.txt', 'w').write('')
-        while True:
-            z = user[i].split('href="/')[1]
-            z = z[:z.find('/')]
-            open('user1.txt', 'a', encoding='utf-8').write(z + '\n')
-
-            img = x.split('img alt="')
-            hh = 0
-            for h in img:
-                if z in h:
-                    try:
-                        driver.find_element_by_xpath(f'/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[{hh - 10}]/div[1]/div/div').click()
-                        sleep(15)
-                        driver.find_element_by_class_name(
-                            '_abl-')
-
-                        driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/div[1]/div/section/div/div[3]/div/div/div[2]/span/button').click()
-                        sleep(1)
-                        print(f'[+] Done Like => {z}')
-                        driver.back()
-                        sleep(5)
-                    except:
-                        pass
-
-                else:
-                    pass
-                hh += 1
-            i += 1
+        z = q['email']
+        print(use)
+        open('gmail.txt', 'a', encoding='utf-8').write(use + '@gmail.com' + '\n')
 
     except:
         pass
-except:
-    pass
-# Wait for the following modal to load
-wait = WebDriverWait(driver, 10)
-wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[role='dialog']")))
 
-# Scrape the users the target user is following
-following_list = driver.find_elements_by_css_selector("#mount_0_0_r0 > div > div > div > div:nth-child(4) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div._aano > div:nth-child(1)")
-following = [user.text for user in following_list]
-print(following)
+def users(x):
 
-# Close the web driver
-driver.close()
+    #x = open('kok.txt', 'r', encoding='utf-8').read()
+    use = x.split("'s profile picture")
+    for i in use:
+        user1.append( i.split('img alt="')[-1])
+
+
+driver4 = webdriver.Chrome(executable_path='chromedriver',options=options, port=port)
+
+
+def login(user, pas):
+    driver4.get("https://www.instagram.com/accounts/login/")
+    sleep(3)
+    username_input = driver4.find_element_by_name("username")
+    password_input = driver4.find_element_by_name("password")
+    username_input.send_keys(user)
+    password_input.send_keys(pas)
+    password_input.submit()
+    sleep(8)
+    print('[+] Done Login')
+    read_aloud('Done Login ')
+
+
+def get_user(u, t):
+    r = -3
+    #u = open('user3.txt', 'r').read().split('\n')[r]
+    driver4.get(f'https://www.instagram.com/{u}/followers/')
+    sleep(4)
+    a = 0
+    while a < t/12:
+        try:
+
+            following_panel = driver4.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]')
+            ht = driver4.execute_script(""" 
+        arguments[0].scrollTo(0, arguments[0].scrollHeight);
+        return arguments[0].scrollHeight; """, following_panel)
+            sleep(2)
+        except:
+            pass
+        a += 1
+
+    try:
+
+        x = driver4.page_source
+        #user = str(x).split('aria-disabled="false"')
+        i = 1
+        while True:
+            user11 = x.split('img alt="')[i].split("'s profile")[0]
+            users(user11)
+            i += 1
+    except:
+        pass
+
+
+def gml():
+    
+    driver = webdriver.Chrome(executable_path='chromedriver',options=options, port=port)
+    read_aloud('Search about gmail ...')
+    print('Search about gmail ...')
+
+    driver.get('https://accounts.google.com/signup/v2/webcreateaccount?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=SignUp')
+    for i in user1:
+        if 'profile picture' in i or '_' in i or ' ' in i or len(i) < 5:
+            pass
+        else:
+            driver.find_element_by_css_selector('#username').click()
+            driver.find_element_by_css_selector('#username').send_keys(i)
+            driver.find_element_by_css_selector('#firstName').click()
+            sleep(3)
+            u = driver.find_element_by_css_selector('#view_container > div > div > div.pwWryf.bxPAYd > div > div.WEQkZc > div > form > span > section > div > div > div.akwVEf.OcVpRe > div.d2CFce.cDSmF.OcVpRe > div > div.LXRPh').text
+            sleep(1)
+            print(u)
+            if 'اسم المستخدم هذا مستخدم بالفعل. جرّب اسم مستخدم آخر' in u:
+                pass
+            else:
+                check(i)
+
+            driver.refresh()
+
+    driver.delete_all_cookies()
+
+
+login('zxcv.kis', 'ahmedcan3')
+
+while True:
+    try:
+        read_aloud('Put The User and Number of Followers')
+        u = input('User Target =>')
+        no = input('No. of Followers => ')
+        get_user(u, int(no))
+        gml()
+    except:
+        pass
